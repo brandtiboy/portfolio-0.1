9d4cbb6707bda481eeb2de4c88056cdb296962c8
Frederik Brandt <frederiksbrandt@gmail.com>
2020-06-23 22:24:05 +0200

    added buttons to cv for mobile version

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-$( document ).ready(function() {
+function myFunction() {
+    var x = document.getElementById("resume_toggle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
-    $( ".cross" ).hide();
-    $( ".menu" ).hide();
-    $( ".hamburger" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".hamburger" ).hide();
-    $( ".cross" ).show();
-    });
-    });
-    
-    $( ".cross" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-    $( ".cross" ).hide();
-    $( ".hamburger" ).show();
-    });
-    });
-    
-});
+  function myFunction1() {
+    var x = document.getElementById("personligt_toggle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("kompetencer_toggle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("uni_toggle");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }

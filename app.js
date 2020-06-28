12a93e818aa575f4724e04890704115ee09d790f
Frederik Brandt <frederiksbrandt@gmail.com>
2020-06-28 13:08:15 +0200

    collapisble containers for mobile version

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("resume_toggle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
   function myFunction1() {
     var x = document.getElementById("personligt_toggle");
     if (x.style.display === "none") {
@@ -33,3 +24,18 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}

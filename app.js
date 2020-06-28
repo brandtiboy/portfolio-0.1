abb4ed3a3d171ad21dc13be888d67830b2c152b4
Frederik Brandt <frederiksbrandt@gmail.com>
2020-06-28 21:43:12 +0200

    removed functions that wasn't in use

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-  function myFunction1() {
-    var x = document.getElementById("personligt_toggle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function myFunction2() {
-    var x = document.getElementById("kompetencer_toggle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  function myFunction3() {
-    var x = document.getElementById("uni_toggle");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
